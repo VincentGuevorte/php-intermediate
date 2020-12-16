632dc09216ded084ae2a52573d0da43a965fb172
VincentGuevorte <v.guevorte@laposte.net>
2020-12-16 15:51:51 +0100

    Création de formulaire

---
+++ public/login.php
@@ -0,0 +1,7 @@
+<?php
+// validation de form d'authentification
+// si l'utilisateur est correctement authentifié :
+// - son login est password sont copiés dans la variable de session 
+// - il est rediriger vers la page private.php
+// sinon un message générique est affiché:
+// "Mot de passe ou login invalide"
